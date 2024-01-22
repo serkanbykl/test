2db76c2924babc6183cfbfca77513946168d1e7f
Serkan <serkanbykl12@gmail.com>
2024-01-22 14:07:59 +0300

    changed custom editor

--- ckeditor.d.ts
+++ ckeditor.d.ts
@@ -8,63 +8,25 @@ import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
 import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
 import type { EditorConfig } from '@ckeditor/ckeditor5-core';
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { FontColor, FontSize } from '@ckeditor/ckeditor5-font';
 import { Heading } from '@ckeditor/ckeditor5-heading';
-import { HtmlComment } from '@ckeditor/ckeditor5-html-support';
-import {
-  AutoImage,
-  Image,
-  ImageCaption,
-  ImageStyle,
-  ImageToolbar,
-  ImageUpload,
-} from '@ckeditor/ckeditor5-image';
+import { HtmlEmbed } from '@ckeditor/ckeditor5-html-embed';
+import { GeneralHtmlSupport, HtmlComment } from '@ckeditor/ckeditor5-html-support';
+import { AutoImage, Image, ImageResize, ImageToolbar, ImageUpload } from '@ckeditor/ckeditor5-image';
 import { Indent } from '@ckeditor/ckeditor5-indent';
-import { AutoLink, Link, LinkImage } from '@ckeditor/ckeditor5-link';
+import { Link, LinkImage } from '@ckeditor/ckeditor5-link';
 import { List } from '@ckeditor/ckeditor5-list';
-import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
+import { MediaEmbed, MediaEmbedToolbar } from '@ckeditor/ckeditor5-media-embed';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
 import { ShowBlocks } from '@ckeditor/ckeditor5-show-blocks';
-import {
-  SpecialCharacters,
-  SpecialCharactersMathematical,
-} from '@ckeditor/ckeditor5-special-characters';
+import { SpecialCharacters, SpecialCharactersMathematical } from '@ckeditor/ckeditor5-special-characters';
 import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
 import { TextTransformation } from '@ckeditor/ckeditor5-typing';
 import { Undo } from '@ckeditor/ckeditor5-undo';
 import { SimpleUploadAdapter } from '@ckeditor/ckeditor5-upload';
 declare class Editor extends ClassicEditor {
-  static builtinPlugins: (
-    | typeof AutoImage
-    | typeof AutoLink
-    | typeof Autoformat
-    | typeof BlockQuote
-    | typeof Bold
-    | typeof Essentials
-    | typeof Heading
-    | typeof HtmlComment
-    | typeof Image
-    | typeof ImageCaption
-    | typeof ImageStyle
-    | typeof ImageToolbar
-    | typeof ImageUpload
-    | typeof Indent
-    | typeof Italic
-    | typeof Link
-    | typeof LinkImage
-    | typeof List
-    | typeof MediaEmbed
-    | typeof Paragraph
-    | typeof PasteFromOffice
-    | typeof ShowBlocks
-    | typeof SimpleUploadAdapter
-    | typeof SpecialCharacters
-    | typeof SpecialCharactersMathematical
-    | typeof Table
-    | typeof TableToolbar
-    | typeof TextTransformation
-    | typeof Undo
-  )[];
-  static defaultConfig: EditorConfig;
+    static builtinPlugins: (typeof AutoImage | typeof Autoformat | typeof BlockQuote | typeof Bold | typeof Essentials | typeof FontColor | typeof FontSize | typeof GeneralHtmlSupport | typeof Heading | typeof HtmlComment | typeof HtmlEmbed | typeof Image | typeof ImageResize | typeof ImageToolbar | typeof ImageUpload | typeof Indent | typeof Italic | typeof Link | typeof LinkImage | typeof List | typeof MediaEmbed | typeof MediaEmbedToolbar | typeof Paragraph | typeof PasteFromOffice | typeof ShowBlocks | typeof SimpleUploadAdapter | typeof SpecialCharacters | typeof SpecialCharactersMathematical | typeof Table | typeof TableToolbar | typeof TextTransformation | typeof Undo)[];
+    static defaultConfig: EditorConfig;
 }
 export default Editor;
